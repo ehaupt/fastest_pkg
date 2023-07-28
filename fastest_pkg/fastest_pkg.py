5d88ef49a8604e714cac45d24db994dca432a049
Emanuel Haupt <ehaupt@critical.ch>
2023-07-28 08:01:38 +0200

    fix: Update example line
    Display mirror_type: "NONE" in example line as recent versions of
    pkg(8) are more capable of warning.
    
    Issue:		https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=272468
    Reported by:	@grahamperrin

--- fastest_pkg/fastest_pkg.py
+++ fastest_pkg/fastest_pkg.py
@@ -146,7 +146,7 @@ def main():
         print(json.dumps(stats_sorted))
     else:
         pkg = PkgMirror(stats_sorted[0]["mirror_name"])
-        pkg_cfg = 'FreeBSD: { url: "http://%s/${ABI}/%s" }' % (
+        pkg_cfg = 'FreeBSD: { url: "http://%s/${ABI}/%s", mirror_type: "NONE" }' % (
             stats_sorted[0]["mirror_name"],
             pkg.release,
         )
